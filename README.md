6aaa475ae8467d417443e0d5950ecb906a2f6775
Edson Junior <edsonkjr@gmail.com>
2023-06-10 19:52:47 -0300

    Projeto finalizado com dashboard da JVM funcionando

---
+++ README.md
@@ -0,0 +1,2 @@
+Tutorial Grafana e Prometheus:
+https://ordina-jworks.github.io/monitoring/2020/11/16/monitoring-spring-prometheus-grafana.html
--- src/main/java/com/example/MonitoringSpringDemoProject/CustomHealthCheck.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/CustomHealthCheck.java
@@ -1,19 +1,17 @@
 package com.example.MonitoringSpringDemoProject;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@AllArgsConstructor
 public class CustomHealthCheck extends AbstractHealthIndicator {
+
     @Override
-    protected void doHealthCheck(Health.Builder builder) throws Exception {
-        boolean running = true;
-        if (running) {
-            builder.up();
-        } else {
-            builder.down();
-        }
+    protected void doHealthCheck(final Health.Builder builder) throws Exception {
+        builder.up().withDetail("app", "WORKING");
     }
 }
