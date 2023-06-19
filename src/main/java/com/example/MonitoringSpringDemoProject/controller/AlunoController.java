0d77cd04ac51e15c1beefe544cae8a3088ceb94a
Edson Junior <edsonkjr@gmail.com>
2023-06-19 19:53:16 -0300

    Implementado post de clientes - ainda com erros

--- src/main/java/com/example/MonitoringSpringDemoProject/controller/AlunoController.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/controller/AlunoController.java
@@ -1,11 +1,14 @@
 package com.example.MonitoringSpringDemoProject.controller;
 
+import com.example.MonitoringSpringDemoProject.domain.Cliente;
 import com.example.MonitoringSpringDemoProject.dto.ClienteDTO;
+import com.example.MonitoringSpringDemoProject.dto.CreateClienteDTO;
 import com.example.MonitoringSpringDemoProject.service.ClienteService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping({"clientes"})
@@ -13,13 +16,25 @@ public class AlunoController {
 
     private final ClienteService clienteService;
 
-    public AlunoController (ClienteService clienteService) {
+    public AlunoController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
 
     @GetMapping(value = {"{id}"}, produces = {"application/json"})
     public ClienteDTO findById(@PathVariable Integer id) {
         return clienteService.findById(id);
+    }
+
+//    @GetMapping(produces = {"application/json"})
+//    public List<ClienteDTO> getAll(@RequestParam(required = false, value = "nome") String nome) {
+//        return clienteService.findAll(nome);
+//    }
+
+
+    @PostMapping(consumes = {"application/json"}, produces = {"application/json"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClienteDTO create(CreateClienteDTO createClienteDTO) {
+        return this.clienteService.create(createClienteDTO);
     }
 
 }
