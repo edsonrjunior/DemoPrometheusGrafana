32a7637e79bcd0c5d8d5d4d620033acb63162f06
Edson Junior <edsonkjr@gmail.com>
2023-06-20 18:38:14 -0300

    Inclusão de validação do tamanho do telfone

--- src/main/java/com/example/MonitoringSpringDemoProject/dto/CreateClienteDTO.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/dto/CreateClienteDTO.java
@@ -1,8 +1,8 @@
 package com.example.MonitoringSpringDemoProject.dto;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,6 +11,8 @@ public class CreateClienteDTO {
 
     private @NotNull(message = "O Id não pode ser nulo") Integer id;
     private @NotBlank(message = "O Nome não pode ser vazio") String nome;
+
+    @Size(min = 11, max = 11, message = "O telefone deve ter 11 dígitos")
     private @NotBlank(message = "O telefone não pode ser vazio") String telefone;
 
 }
