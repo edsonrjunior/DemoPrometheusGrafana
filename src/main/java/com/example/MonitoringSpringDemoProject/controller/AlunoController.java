46afe531eb676d46d86670c1a3840fbf74ce3406
Edson Junior <edsonkjr@gmail.com>
2023-06-20 15:39:02 -0300

    Adicionando validação no post de clientes

--- src/main/java/com/example/MonitoringSpringDemoProject/controller/AlunoController.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/controller/AlunoController.java
@@ -5,7 +5,12 @@ import com.example.MonitoringSpringDemoProject.dto.CreateClienteDTO;
 import com.example.MonitoringSpringDemoProject.service.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping({"clientes"})
@@ -32,6 +37,21 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public ClienteDTO create(@RequestBody @Valid CreateClienteDTO createClienteDTO) {
         return this.clienteService.create(createClienteDTO);
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public Map<String, String> handlerValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 
 }
--- src/main/java/com/example/MonitoringSpringDemoProject/dto/CreateClienteDTO.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/dto/CreateClienteDTO.java
@@ -1,5 +1,6 @@
 package com.example.MonitoringSpringDemoProject.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -7,8 +8,9 @@ import lombok.Data;
 @Data
 public class CreateClienteDTO {
 
-    private @NotNull Integer id;
-    private @NotBlank String nome;
-    private @NotBlank String telefone;
+
+    private @NotNull(message = "O Id não pode ser nulo") Integer id;
+    private @NotBlank(message = "O Nome não pode ser vazio") String nome;
+    private @NotBlank(message = "O telefone não pode ser vazio") String telefone;
 
 }
