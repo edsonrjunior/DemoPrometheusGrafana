6763e24f9a3658d4e4e4a7a75a42301ee7144a2a
Edson Junior <edsonkjr@gmail.com>
2023-12-11 00:42:45 -0300

    Implementando Swagger e atualizando Spring Boot para 3.2.0

--- src/main/java/com/example/MonitoringSpringDemoProject/DemoApplication.java
+++ src/main/java/com/example/MonitoringSpringDemoProject/DemoApplication.java
@@ -1,9 +1,31 @@
 package com.example.MonitoringSpringDemoProject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Projeto teste de observability",
+				version = "1.0.0",
+				description = "Projeto para fins de estudo",
+				termsOfService = "edsonrjk",
+				contact = @Contact(
+						name = "Edson Jr",
+						email = "edsonkjk@gmail.com"
+				),
+				license = @License(
+						name = "licence",
+						url = "edsonkjk"
+				)
+
+
+		)
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
